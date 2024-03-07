3ead836f811a67d71c25c66060672dbd8fc41f13
vahand <vahan.ducher@epitech.eu>
2024-03-07 09:06:56 +0100

    [feat] add read file function (return IO String)

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main (main) where
 
 import Lib
+import Parser
+
+printLines :: [String] -> IO ()
+printLines [] = return ()
+printLines (x:xs) = putStrLn x >> printLines xs
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Hello, Haskell!"
+    parse "test.txt"
+    putStrLn "End of program"
